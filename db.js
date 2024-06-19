7a5e66f58be65375399c1d726f2904dd4fc8ec2a
Robert Yohnka <robertyohnka@Roberts-MBP.attlocal.net>
2024-06-19 00:43:19 -0400

    backending

---
+++ db.js
@@ -0,0 +1,147 @@
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_store_db');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT || 'shhh';
+if (JWT === 'shhh') {
+    console.log('If deployed, set process.env.JWT to something other than shhh');
+}
+
+const createTables = async () => {
+    const SQL = `
+        DROP TABLE IF EXISTS assignments;
+        DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS departments;
+        CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+        jobTitle VARCHAR(20),
+        email EMAIL,
+        phoneNumber VARCHAR(10),
+        );
+        CREATE TABLE departments(
+        id UUID PRIMARY KEY,
+        name VARCHAR(20) UNIQUE NOT NULL,
+        deptChair VARCHAR(20),
+        deptChairEmail EMAIL,
+        deptChairPhone VARCHAR(10),
+        deptAdmin VARCHAR(20),
+        deptAdminEmail EMAIL,
+        deptAdminPhone VARCHAR(10),
+        schoolName VARCHAR(20),
+        );
+        CREATE TABLE assignments(
+        id UUID PRIMARY KEY,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        dept_id UUID REFERENCES departments(id) NOT NULL,
+        CONSTRAINT unique_user_id_and_dept_id UNIQUE (user_id, dept_id)
+        );
+    `;
+    await client.query(SQL);
+};
+
+const createUser = async ({ username, password, jobTitle, email, phoneNumber }) => {
+    const SQL = `
+        INSERT INTO users(id, username, password, jobTitle, email, phoneNumber) VALUES($1, $2, $3, $4, $5, $6) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5), jobTitle, email, phoneNumber]);
+    return response.rows[0];
+};
+
+const createDept = async ({ name, deptChair, deptChairEmail, deptChairPhone, deptAdmin, deptAdminEmail, deptAdminPhone, schoolName }) => {
+    const SQL = `
+        INSERT INTO departments(id, name, deptChair, deptChairEmail, deptChairPhone, deptAdmin, deptAdminEmail, deptAdminPhone, schoolName) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name, deptChair, deptChairEmail, deptChairPhone, deptAdmin, deptAdminEmail, deptAdminPhone, schoolName]);
+    return response.rows[0];
+};
+
+const createAssignment = async ({ user_id, dept_id }) => {
+    const SQL = `
+        INSERT INTO assignments(id, user_id, dept_id) VALUES($1, $2, $3) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), user_id, dept_id]);
+    return response.rows[0];
+};
+
+const destroyAssignment = async ({ user_id, id }) => {
+    const SQL = `
+        DELETE FROM assignments WHERE user_id=$1 AND id=$2
+    `;
+    await client.query(SQL, [user_id, id]);
+};
+
+const authenticate = async ({ username, password }) => {
+    const SQL = `
+        SELECT id, username, password FROM users WHERE username=$1;
+    `;
+    const response = await client.query(SQL, [username]);
+    if (response.rows.length || (await bcrypt.compare(password, response.rows[0].password)) === false) {
+        const error = Error('bad credentials');
+        error.status = 401;
+        throw error;
+    }
+    const token = await jwt.sign({ id: response.rows[0].id }, JWT);
+    return { token };
+};
+
+const findUserWithToken = async (token) => {
+    let id;
+    try {
+        const payload = await jwt.verify(token, JWT);
+        id = payload.id;
+    }
+    catch (ex) {
+        const error = Error('bad token');
+        error.status = 401;
+        throw error;
+    }
+    const SQL = `
+        SELECT id, username FROM users WHERE id=$1
+    `;
+    const response = await client.query(SQL, [id]);
+    if (!response.rows.length) {
+        const error = Error('bad token');
+        error.status = 401;
+        throw error;
+    }
+    return response.rows[0];
+};
+
+const fetchUsers = async () => {
+    const SQL = `
+        SELECT id, username FROM users;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+
+const fetchDepartments = async () => {
+    const SQL = `
+        SELECT id, name FROM departments;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+
+const fetchAssignments = async (user_id) => {
+    const SQL = `
+        SELECT * FROM assignments WHERE user_id=$1
+    `;
+    const response = await client.query(SQL, [user_id]);
+    return response.rows;
+};
+
+module.exports = {
+    createUser,
+    createDept,
+    createAssignment,
+    destroyAssignment,
+    authenticate,
+    findUserWithToken,
+    fetchUsers,
+    fetchDepartments,
+    fetchAssignments
+};
---
+++ index.js
@@ -0,0 +1,111 @@
+const {
+    client,
+    createTables,
+    createUser,
+    createDept,
+    createAssignment,
+    destroyAssignment,
+    authenticate,
+    findUserWithToken,
+    fetchUsers,
+    fetchDepartments,
+    fetchAssignments
+} = require('./db');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/api/users', async (req, res, next) => {
+    try {
+        res.send(await fetchUsers());
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.get('/api/departments', async (req, res, next) => {
+    try {
+        res.send(await fetchDepartments());
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.get('/api/assignments/:user_id', async (req, res, next) => {
+    try {
+        res.send(await fetchAssignments());
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.post('api/users/:user_id/assignments', async (req, res, next) => {
+    try {
+        res.status(201).send(await createAssignment({ user_id: req.params.user_id, dept_id: req.body.dept_id }));
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.delete('/api/users/:user_id/assignments/:id', async (req, res, next) => {
+    try {
+        await destroyAssignment({ id: req.params.id, user_id: req.params.user_id });
+        res.sendStatus(204);
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).send({ error: err.message || err });
+});
+
+const init = async () => {
+    console.log('connecting to the database');
+    // const port = process.env.PORT || 3000;
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    console.log('tables created');
+
+    const [chris, hayli, amanda, sarah, emma, fran, jill, pathology, dermatology, urology, emergMedicine, neurology, obGyn] = await Promise.all([
+        createUser({ username: 'chris', password: 'ch_pw' }),
+        createUser({ username: 'hayli', password: 'ha_pw' }),
+        createUser({ username: 'amanda', password: 'am_pw' }),
+        createUser({ username: 'sarah', password: 'sa_pw' }),
+        createUser({ username: 'emma', password: 'em_pw' }),
+        createUser({ username: 'fran', password: 'fr_pw' }),
+        createUser({ username: 'jill', password: 'ji_pw' }),
+        createDept({ name: 'pathology' }),
+        createDept({ name: 'dermatology' }),
+        createDept({ name: 'urology' }),
+        createDept({ name: 'emergMedicine' }),
+        createDept({ name: 'neurology' }),
+        createDept({ name: 'obGyn' }),
+    ]);
+
+    console.log(await fetchUsers());
+    console.log(await fetchDepartments());
+
+    const [assignment1, assignment2, assignment3, assignment4, assignment5, assignment6] = await Promise.all([
+        createAssignment({ user_id: chris.id, dept_id: pathology.id }),
+        createAssignment({ user_id: hayli.id, dept_id: dermatology.id }),
+        createAssignment({ user_id: amanda.id, dept_id: urology.id }),
+        createAssignment({ user_id: sarah.id, dept_id: emergMedicine.id }),
+        createAssignment({ user_id: emma.id, dept_id: neurology.id }),
+        createAssignment({ user_id: fran.id, dept_id: obGyn.id }),
+    ]);
+
+    console.log(await fetchAssignments());
+    await destroyAssignment({ id: assignment1.id, user_id: assignment1.user_id });
+    console.log(await fetchAssignments());
+    const assignment = await createAssignment({ user_id: chris.id, dept_id: pathology.id });
+    app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
